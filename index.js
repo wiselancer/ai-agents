dec7b6d5d256d743b25fd8045e4cd8dc5a358005
Wiselancer <310760+wiselancer@users.noreply.github.com>
2024-09-30 15:51:31 +0800

    feat: Add function to render new messages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import OpenAI from "openai"
 import { getCurrentWeather, getLocation, functions } from "./tools"
+import { renderNewMessage } from "./dom"
 
 export const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
@@ -11,20 +12,41 @@ const availableFunctions = {
     getLocation
 }
 
-async function agent(query) {
-    const messages = [
-        { role: "system", content: "You are a helpful AI agent. Give highly specific answers based on the information you're provided. Prefer to gather information with the tools provided to you rather than giving basic, generic answers." },
-        { role: "user", content: query }
-    ]
+const messages = [
+    {
+        role: "system", content: `
+You are a helpful AI agent. Transform technical data into engaging, 
+conversational responses, but only include the normal information a 
+regular person might want unless they explicitly ask for more. Provide 
+highly specific answers based on the information you're given. Prefer 
+to gather information with the tools provided to you rather than 
+giving basic, generic answers.
+`
+    },
+]
 
-    const runner = openai.beta.chat.completions.runTools({
-        model: "gpt-3.5-turbo-1106",
+async function agent(query) {
+
+    messages.push({ role: "user", content: query })
+    renderNewMessage(query, "user")
+
+    const runner = openai.beta.chat.completions.runFunctions({
+        model: "gpt-4-1106-preview",
         messages,
         functions
     }).on("message", (message) => console.log(message))
-    
+
     const finalContent = await runner.finalContent()
-    console.log(finalContent)
+    messages.push({ role: "system", content: finalContent })
+    renderNewMessage(finalContent, "assistant")
 }
 
-await agent("What's the current weather in my current location?")
+document.getElementById("form").addEventListener("submit", async function (event) {
+    event.preventDefault()
+    const inputElement = document.getElementById("user-input")
+    inputElement.focus()
+    const formData = new FormData(event.target)
+    const query = formData.get("user-input")
+    event.target.reset()
+    await agent(query)
+})
